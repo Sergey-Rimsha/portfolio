b3a434513628000b50554b3323cd63e82343606d
Sergey <Sergey.rimsha.ru@gmail.com>
2022-05-12 00:48:05 +0300

    font style header

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -8,12 +8,19 @@ export const Main = (props) => {
 			<div className={props.styleContainer}>
 				<div className={styles.contentWrap}>
 					<div className={styles.mainInfo}>
-						<h1>Привет!</h1>
-						<div>Меня зовут Сергей</div>
-						<span>Я Frontend Developers</span>
+						<h1>
+							I`m Sergey Rimsha
+							<br/>
+							<span>
+								Frontend Developer.
+							</span>
+						</h1>
+						<button>
+							<a href='' download=''>download CV</a>
+						</button>
 					</div>
 					<div className={styles.mainPhoto}>
-						<img src={''} alt={'photo'}/>
+						<img src='../../assets/ava_main.jpg' alt='photo'/>
 					</div>
 				</div>
 			</div>
--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -6,10 +6,19 @@ export const Nav = () => {
 	return (
 		<div className={styles.navWrap}>
 			<nav className={styles.nav}>
-				<a href={``}>Главная</a>
-				<a href={``}>Скилы</a>
-				<a href={``}>Работа</a>
-				<a href={``}>Контакты</a>
+				<div className={styles.nav__link}>
+					<a href={``}>Главная</a>
+				</div>
+				<div className={styles.nav__link}>
+					<a href={``}>Скилы</a>
+				</div>
+				<div className={styles.nav__link}>
+					<a href={``}>Работа</a>
+				</div>
+				<div className={styles.nav__link}>
+					<a href={``}>Контакты</a>
+				</div>
+
 			</nav>
 		</div>
 	)
